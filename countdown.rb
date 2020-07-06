8992f545f8152002b2ddd3979ab505764579a324
Wick Bushong <wickbushong@gmail.com>
2020-07-06 18:13:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,14 @@ def countdown (num)
   while num > 0
     puts "#{num} SECOND(S)!"
     num -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
     sleep(1)
   end
   "HAPPY NEW YEAR!"
